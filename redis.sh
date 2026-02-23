6a77dcf27a1f1a23d73d0f809e54ede3d8584431
Rajesh <katamnenirajesh2.gmail.com>
2026-02-24 00:11:19 +0530

    actions

--- redis.sh
+++ redis.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 USERID=$(id -u)
-LOGS_FOLDER="/var/log/shell-script"
-LOGS_FILE="/var/log/shell-script/$0.log"
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
