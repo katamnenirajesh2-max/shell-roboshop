0535c9c8c3ecd9f3a57a962339aabb91f0dd1ed2
Rajesh <katamnenirajesh2.gmail.com>
2026-02-23 16:54:13 +0530

    actions

--- roboshop.sh
+++ roboshop.sh
@@ -2,34 +2,31 @@
 
 SG_ID="sg-0ee2710ed1ccb428d"
 AMI_ID="ami-0220d79f3f480ecf5"
+DOMAIN_NAME="daws88s.online"
 
-for instance in $@
- do
+for instance in "$@"
+do
+    INSTANCE_ID=$(aws ec2 run-instances \
+        --image-id $AMI_ID \
+        --instance-type "t3.micro" \
+        --security-group-ids $SG_ID \
+        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+        --query 'Instances[0].InstanceId' \
+        --output text)
 
-    INSTANCE_ID=$( aws ec2 run-instances \
-    --image-id $AMI_ID \
-    --instance-type "t3.micro" \
-    --security-group-ids $SG_ID \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
-    --query 'Instances[0].InstanceId' \
-    --output text )
-
-    if [ $instance == "frontend" ]; then
-        IP=$(
-            aws ec2 describe-instances \
+    if [ "$instance" == "frontend" ]; then
+        IP=$(aws ec2 describe-instances \
             --instance-ids $INSTANCE_ID \
             --query 'Reservations[].Instances[].PublicIpAddress' \
-            --output text
-        )
-        RECORD_NAME="$DOMAIN_NAME" # daws88s.online
+            --output text)
+
+        RECORD_NAME="$DOMAIN_NAME"
     else
-        IP=$(
-            aws ec2 describe-instances \
+        IP=$(aws ec2 describe-instances \
             --instance-ids $INSTANCE_ID \
             --query 'Reservations[].Instances[].PrivateIpAddress' \
-            --output text
-        )
-        
+            --output text)
+
         echo "IP Address: $IP"
-   
-   done  
+    fi
+done
