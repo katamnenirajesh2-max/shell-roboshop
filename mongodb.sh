56ea5244768588dca256442692678b1e59a8e295
Rajesh <katamnenirajesh2.gmail.com>
2026-02-23 20:00:34 +0530

    actions

--- mongodb.sh
+++ mongodb.sh
@@ -40,4 +40,4 @@ sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE $? "Allowing remote connections"
 
 systemctl restart mongod
-VALIDATE $? "Restarted MongoDB"
+VALIDATE $? "Restarted MongoDB"S
